ef7bfc3ec50ed08d13da7556ecc0802cf9f25a90
jagrutiburde0413 <124602129+jagrutiburde0413@users.noreply.github.com>
2023-04-27 22:42:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function displayTimer(){
 	var date=new Date();
 	var time=date.toLocaleTimeString();
 	var option={
-		day:'numeric',month:'long',year:'numeric'
+		day:'numeric',month:'numeric',year:'numeric'
 	}
 	var today=date.toLocaleDateString(undefined, option);
-	document.getElementById("timer").innerHTML=today+time;
+	document.getElementById("timer").innerHTML=today+", "+time;
 }
 setInterval(displayTimer,1000)
